6828e74e3cb2d2104aa94acef73e1a05cec6b62f
Sanmiguel2420 <128133617+Sanmiguel2420@users.noreply.github.com>
2024-01-24 19:43:04 +0100

    Add files via upload

---
+++ PHPDOCKER/app/config_BD.php
@@ -0,0 +1,10 @@
+<?php
+
+// Parámetros acceso a una BD
+define('HOST', '192.168.1.132');
+define('BD', 'Despliegue');
+define('USER', 'kiko');
+define('PASSWORD', 'kiko');
+define('CHARSET', 'utf8');
+
+?>
